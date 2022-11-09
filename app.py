23234f259088a8a31249c71f6fd054e0d65a0a3f
kylerequez <kyle.requez.code@gmail.com>
2022-11-09 23:00:25 +0800

    Implemented Backlog

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, request, render_template
-import ipapi
+from flask import Flask, request, render_template, redirect
+from requests import get
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
-    ip = request.form.get('ip')
-    data = ipapi.location(ip=ip, output='json')
-    return render_template('index.html', data=data)
+    if request.method == "POST":
+        ip = request.form.get('ip')
+        data = get('https://ipapi.co/' + ip + '/json/').json()
+        return render_template('index.html', data=data)
+    elif request.method == "GET":
+        if request.args != '':
+            ip = request.args
+            data = get('https://ipapi.co/' + ip[1] + '/json/').json()
+            return render_template('index.html', data=data)
+        else:
+            return render_template('index.html')
+            
 
 if __name__ == "__main__":
     app.run(debug=True)
