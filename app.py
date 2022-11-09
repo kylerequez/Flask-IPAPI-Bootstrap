70d733cc5092f525b4e6d2322fe0da24e0cd738d
kylerequez <kyle.requez.code@gmail.com>
2022-11-09 23:58:03 +0800

    Improved Functionality and Front End Design

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from requests import get
 
 app = Flask(__name__)
 
+addresses = []
+
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
         ip = request.form.get('ip')
         data = get('https://ipapi.co/' + ip + '/json/').json()
-        return render_template('index.html', data=data)
+        addresses.append([ip, '?ip=' + ip])
+        return render_template('index.html', data=data, addresses=addresses)
     elif request.method == "GET":
-        if request.args != '':
-            ip = request.args
-            data = get('https://ipapi.co/' + ip[1] + '/json/').json()
-            return render_template('index.html', data=data)
+        args = request.args
+        if "ip" in args:
+            ip = args["ip"]
+            data = get('https://ipapi.co/' + ip + '/json/').json()
+            return render_template('index.html', data=data, addresses=addresses)
         else:
-            return render_template('index.html')
+            return render_template('index.html', addresses=addresses)
             
-
 if __name__ == "__main__":
     app.run(debug=True)
